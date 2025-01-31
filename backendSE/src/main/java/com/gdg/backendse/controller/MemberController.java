bb85c569403505a567b5dda6870ec4ac43b5765e
JeongBeanHyun <kelly92108@gmail.com>
2025-02-01 02:22:55 +0900

    google 소셜로그인

---
+++ backendSE/src/main/java/com/gdg/backendse/controller/MemberController.java
@@ -0,0 +1,22 @@
+package com.gdg.backendse.controller;
+
+import com.gdg.backendse.domain.Member;
+import com.gdg.backendse.service.GoogleLoginService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("member")
+public class MemberController {
+    private final GoogleLoginService googleLoginService;
+
+    @GetMapping("/test")
+    public Member member(Principal principal){
+        return googleLoginService.test(principal);
+    }
+}
