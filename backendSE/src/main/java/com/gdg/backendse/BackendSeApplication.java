bb80dc25a98767148fc79e36088ee0bd620615b5
AjinKimmm <kocuya6315@naver.com>
2025-02-06 14:45:35 +0900

    db명변경, 홈추가

--- backendSE/src/main/java/com/gdg/backendse/BackendSeApplication.java
+++ backendSE/src/main/java/com/gdg/backendse/BackendSeApplication.java
@@ -2,12 +2,14 @@ package com.gdg.backendse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.gdg.backendse")
 public class BackendSeApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BackendSeApplication.class, args);
     }
-
 }
