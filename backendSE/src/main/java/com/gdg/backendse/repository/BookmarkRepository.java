cadb0b490148fff928d4b012dc1383da15ee29e2
JeongBeanHyun <kelly92108@gmail.com>
2025-02-23 14:35:23 +0900

    fix: 회원탈퇴시 북마크 삭제

--- backendSE/src/main/java/com/gdg/backendse/repository/BookmarkRepository.java
+++ backendSE/src/main/java/com/gdg/backendse/repository/BookmarkRepository.java
@@ -3,14 +3,22 @@ package com.gdg.backendse.repository;
 import com.gdg.backendse.domain.Bookmark;
 import com.gdg.backendse.domain.Edu;
 import com.gdg.backendse.domain.Member;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     List<Bookmark> findByMember(Member member);
-    Optional<Bookmark> findByMemberAndEdu(Member member, Edu edu);
     boolean existsByMemberAndEdu(Member member, Edu edu);
     void deleteByMemberAndEdu(Member member, Edu edu);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Bookmark b WHERE b.member.memberId = :memberId")
+    void deleteByMemberId(@Param("memberId") int memberId);
 }
